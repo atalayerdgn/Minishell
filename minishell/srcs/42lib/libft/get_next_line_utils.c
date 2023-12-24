7bf657907fe7f78fd41e1c1e833fa33910518072
Atalay Erdoğan <110429589+atalayerdgn@users.noreply.github.com>
2023-12-24 06:00:25 +0300

    Add files via upload

---
+++ minishell/srcs/42lib/libft/get_next_line_utils.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aerdogan <aerdogan@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/27 15:17:56 by aerdogan          #+#    #+#             */
+/*   Updated: 2023/07/27 15:18:09 by aerdogan         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_free_stash(char **stash, int create_line)
+{
+	char	*line;
+
+	if (!*stash)
+		return (NULL);
+	if (create_line == 0)
+	{
+		if (*stash)
+		{
+			free(*stash);
+			*stash = NULL;
+		}
+		return (NULL);
+	}
+	else if (create_line == 1)
+	{
+		line = ft_strdup(*stash);
+		free(*stash);
+		*stash = NULL;
+		return (line);
+	}
+	return (NULL);
+}
